b41eadabf8df00a8f1d7ddc4632780e47af0f98b
Penny <ethanexpat@gmail.com>
2022-02-20 15:27:54 -0500

    mpkoewfsjpiokfrew

---
+++ UserESP/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MelonLoader;
+
+namespace UserESP
+{
+    public static class BuildShit
+    {
+        public const string Name = "UserESP";
+        public const string Author = "Penny";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/PennyBunny/VRCMods/";
+        public const string Description = "A standalone mod to turn on a ESP for selected user";
+    }
+
+    public class Main : MelonMod
+    {
+        internal static readonly MelonLogger.Instance log = new MelonLogger.Instance(BuildShit.Name, ConsoleColor.Yellow);
+
+        public override void OnApplicationStart()
+        {
+            log.Msg("Ur gay");
+        }
+    }
+}
--- UserESP/Properties/AssemblyInfo.cs
+++ UserESP/Properties/AssemblyInfo.cs
@@ -1,35 +1,21 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using MelonLoader;
+using UserESP;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UserESP")]
-[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyTitle(BuildShit.Name)]
+[assembly: AssemblyDescription(BuildShit.Description)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UserESP")]
+[assembly: AssemblyProduct(BuildShit.Name)]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8A10A4F5-F591-47C7-9829-6EFF56EF7233")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("788094c8-83cf-4c9b-920c-536bbad2b009")]
+[assembly: AssemblyVersion(BuildShit.Version)]
+[assembly: AssemblyFileVersion(BuildShit.Version)]
+[assembly: MelonInfo(typeof(UserESP.Main), BuildShit.Name, BuildShit.Version, BuildShit.Author, BuildShit.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
