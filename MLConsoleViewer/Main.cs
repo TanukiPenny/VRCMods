c82ca091ba614128d7a35696ff21cb08d20f987e
Penny <ethanexpat@gmail.com>
2022-02-25 17:10:35 -0500

    Oooooooooo

---
+++ MLConsoleViewer/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using MelonLoader;
+
+namespace MLConsoleViewer
+{
+    public static class BuildShit
+    {
+        public const string Name = "MLConsoleViewer";
+        public const string Author = "Penny, Davi";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/PennyBunny/VRCMods/";
+        public const string Description = "";
+    }
+    public class Main : MelonMod
+    {
+        internal static readonly MelonLogger.Instance log = new MelonLogger.Instance(BuildShit.Name, ConsoleColor.Cyan);
+        private static int scenesLoaded = 0;
+        
+        public override void OnApplicationStart()
+        {
+            LoadRemodCore(out _);
+            //BundleManager.InIt();
+            log.Msg("MLConsoleViewer Loaded");
+            log.Msg("Echo....... *there was no response* :(");
+        }
+        private void LoadRemodCore(out Assembly loadedAssembly)
+        {
+            byte[] bytes = null;
+            var wc = new WebClient();
+        
+
+            try
+            {
+                bytes = wc.DownloadData($"https://github.com/RequiDev/ReModCE/releases/latest/download/ReMod.Core.dll");
+                loadedAssembly = Assembly.Load(bytes);
+            }
+            catch (WebException e)
+            {
+                MelonLogger.Error($"Unable to Load Core Dep RemodCore: {e}");
+            }
+            catch (BadImageFormatException e)
+            {
+                loadedAssembly = null;
+            }
+            loadedAssembly = null;
+        }
+    }
+}
---
+++ MLConsoleViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using MLConsoleViewer;
+
+
+[assembly: AssemblyTitle(BuildShit.Name)]
+[assembly: AssemblyDescription(BuildShit.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(BuildShit.Name)]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("788094c8-83cf-4c9b-920c-536bbad2b009")]
+[assembly: AssemblyVersion(BuildShit.Version)]
+[assembly: AssemblyFileVersion(BuildShit.Version)]
+[assembly: MelonInfo(typeof(MLConsoleViewer.Main), BuildShit.Name, BuildShit.Version, BuildShit.Author, BuildShit.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonOptionalDependencies("ReMod.Core")]
