fdf78c0587f548d4ccc214d77297ea038e9eb569
Davivi <48662592+d-magit@users.noreply.github.com>
2022-02-26 04:36:58 -0300

    Update BundleManager.cs

--- MLConsoleViewer/BundleManager.cs
+++ MLConsoleViewer/BundleManager.cs
@@ -11,15 +11,15 @@ namespace MLConsoleViewer
         public static Sprite console;
         public static GameObject prefab;
 
+            
         public static void Init()
         {
             using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MLConsoleViewer.console");
             using var memoryStream = new MemoryStream((int)stream!.Length);
             stream.CopyTo(memoryStream);
             Bundle = AssetBundle.LoadFromMemory_Internal(memoryStream.ToArray(), 0);
-            console = Bundle.LoadAsset_Internal(Bundle.GetAllAssetNames()[0], Il2CppType.Of<Sprite>()).Cast<Sprite>();
-            prefab = Bundle.LoadAsset_Internal(Bundle.GetAllAssetNames()[1], Il2CppType.Of<GameObject>()).Cast<GameObject>();
-            Bundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            (console = Bundle.LoadAsset_Internal(Bundle.GetAllAssetNames()[0], Il2CppType.Of<Sprite>()).Cast<Sprite>()).hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            (prefab = Bundle.LoadAsset_Internal(Bundle.GetAllAssetNames()[1], Il2CppType.Of<GameObject>()).Cast<GameObject>()).hideFlags |= HideFlags.DontUnloadUnusedAsset;
         }
     }
 }
