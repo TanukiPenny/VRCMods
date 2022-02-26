96f6dea2e1b1c29bbcb2b41535318d27df9937a4
Davi <48662592+d-magit@users.noreply.github.com>
2022-02-26 19:49:30 -0300

    Update ConsoleManager.cs

--- MLConsoleViewer/ConsoleManager.cs
+++ MLConsoleViewer/ConsoleManager.cs
@@ -1,25 +1,46 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using Il2CppSystem.Collections.Generic;
 using MelonLoader;
 using UnityEngine;
 
 namespace MLConsoleViewer;
 
-public class ConsoleManager
+public static class ConsoleManager
 {
     public static void AttachTrackers()
     {
         MelonLogger.MsgCallbackHandler += OnMsg;
     }
-
-    public static void AddMsg(ConsoleColor melonColor, ConsoleColor txtColor, string callingMod, string logText)
-    {   
-        UI.text.text += $"[<color=green>{DateTime.Now.AddMilliseconds(-1.0).ToString("HH:mm:ss.fff")}</color>]" +
-                        $" [<color={melonColor.ToString()}>{callingMod}</color>]" +
-                        $" <color={txtColor.ToString()}>{logText}</color>\n";
-    }
-    public static void OnMsg(ConsoleColor melonColor, ConsoleColor txtColor, string callingMod, string logText)
+    private static void OnMsg(ConsoleColor melonColor, ConsoleColor txtColor, string callingMod, string logText)
     {
         AddMsg(melonColor, txtColor, callingMod, logText);
     }
+    private static void AddMsg(ConsoleColor melonColor, ConsoleColor txtColor, string callingMod, string logText)
+    {   
+        UI.text.text += $"[<color=green>{DateTime.Now.AddMilliseconds(-1.0).ToString("HH:mm:ss.fff")}</color>]" +
+                        $" [<color={HexStrings[melonColor]}>{callingMod}</color>]" +
+                        $" <color={HexStrings[txtColor]}>{logText}</color>\n";
+    }
+    private static readonly Hashtable HexStrings = new()
+    {
+        {ConsoleColor.Black, "#000000"},
+        {ConsoleColor.DarkBlue, "#00008b"},
+        {ConsoleColor.DarkGreen, "#008b00"},
+        {ConsoleColor.DarkCyan, "#008b8b"},
+        {ConsoleColor.DarkRed, "#8b0000"},
+        {ConsoleColor.DarkMagenta, "#8b008b"},
+        {ConsoleColor.DarkYellow, "#8b8b00"},
+        {ConsoleColor.Gray, "#c0c0c0"},
+        {ConsoleColor.DarkGray, "#8b8b8b"},
+        {ConsoleColor.Blue, "#0000ff"},
+        {ConsoleColor.Green, "#00ff00"},
+        {ConsoleColor.Cyan, "#00ffff"},
+        {ConsoleColor.Red, "#ff0000"},
+        {ConsoleColor.Magenta, "#ff00ff"},
+        {ConsoleColor.Yellow, "#ffff00"},
+        {ConsoleColor.White, "#ffffff"}
+    };
 }
