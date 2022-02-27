12162c76523b60d444e086bea75dcbf1c3190df4
Penny <ethanexpat@gmail.com>
2022-02-26 19:11:29 -0500

    undoing davi's fucky wucky

--- MLConsoleViewer/Main.cs
+++ MLConsoleViewer/Main.cs
@@ -3,72 +3,70 @@ using System.Net;
 using System.Reflection;
 using MelonLoader;
 
+namespace MLConsoleViewer;
 
-namespace MLConsoleViewer
+public static class BuildShit
 {
-    public static class BuildShit
+    public const string Name = "MLConsoleViewer";
+    public const string Author = "Penny, Davi";
+    public const string Version = "1.0.0";
+    public const string DownloadLink = "https://github.com/PennyBunny/VRCMods/";
+    public const string Description = "";
+}
+public class Main : MelonMod
+{
+    // ReSharper disable once InconsistentNaming
+    internal static readonly MelonLogger.Instance log = new(BuildShit.Name, ConsoleColor.Cyan);
+    private static int scenesLoaded;
+    private static MelonPreferences_Category _mlConsoleViewer;
+    private static MelonPreferences_Entry<int> _fontSize;
+    public override void OnApplicationStart()
     {
-        public const string Name = "MLConsoleViewer";
-        public const string Author = "Penny, Davi";
-        public const string Version = "1.0.0";
-        public const string DownloadLink = "https://github.com/PennyBunny/VRCMods/";
-        public const string Description = "";
+        LoadReModCore(out _);
+        BundleManager.Init();
+        ConsoleManager.AttachTrackers();
+        _mlConsoleViewer = MelonPreferences.CreateCategory("MLConsoleViewer", "MLConsoleViewer");
+        _fontSize = _mlConsoleViewer.CreateEntry("fontSize", 20, "Font Size",
+            "Font size of the text in your console tab");
+        log.Msg("MLConsoleViewer Loaded");
     }
-    public class Main : MelonMod
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
     {
-        internal static readonly MelonLogger.Instance log = new MelonLogger.Instance(BuildShit.Name, ConsoleColor.Cyan);
-        private static int scenesLoaded = 0;
-        public static MelonPreferences_Category MLConsoleViewer;
-        public static MelonPreferences_Entry<int> fontSize;
-        public override void OnApplicationStart()
+        if (scenesLoaded <= 2)
         {
-            LoadRemodCore(out _);
-            BundleManager.Init();
-            MLConsoleViewer = MelonPreferences.CreateCategory("MLConsoleViewer", "MLConsoleViewer");
-            fontSize = MLConsoleViewer.CreateEntry("fontSize", 20, "Font Size",
-                "Font size of the text in your console tab");
-            log.Msg("MLConsoleViewer Loaded");
-            log.Msg("Echo....... *there was no response* :(");
-        }
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
-        {
-            if (scenesLoaded <= 2)
+            scenesLoaded++;
+            if (scenesLoaded == 2)
             {
-                scenesLoaded++;
-                if (scenesLoaded == 2)
-                {
-                    MelonCoroutines.Start(UI.OnQuickMenu());
-                }
+                MelonCoroutines.Start(UI.OnQuickMenu());
             }
         }
+    }
 
-        public override void OnPreferencesSaved()
+    public override void OnPreferencesSaved()
+    {
+        if (UI.text == null)
+            return;
+        UI.text.fontSize = _fontSize.Value;
+    }
+
+    private void LoadReModCore(out Assembly loadedAssembly)
+    {
+        byte[] bytes;
+        var wc = new WebClient();
+            
+        try
         {
-            if (UI.text == null)
-                return;
-            UI.text.fontSize = fontSize.Value;
+            bytes = wc.DownloadData($"https://github.com/RequiDev/ReModCE/releases/latest/download/ReMod.Core.dll");
+            loadedAssembly = Assembly.Load(bytes);
         }
-
-        private void LoadRemodCore(out Assembly loadedAssembly)
+        catch (WebException e)
         {
-            byte[] bytes = null;
-            var wc = new WebClient();
-        
-
-            try
-            {
-                bytes = wc.DownloadData($"https://github.com/RequiDev/ReModCE/releases/latest/download/ReMod.Core.dll");
-                loadedAssembly = Assembly.Load(bytes);
-            }
-            catch (WebException e)
-            {
-                MelonLogger.Error($"Unable to Load Core Dep RemodCore: {e}");
-            }
-            catch (BadImageFormatException e)
-            {
-                loadedAssembly = null;
-            }
+            MelonLogger.Error($"Unable to Load Core Dep RemodCore: {e}");
+        }
+        catch (BadImageFormatException)
+        {
             loadedAssembly = null;
         }
+        loadedAssembly = null;
     }
 }
