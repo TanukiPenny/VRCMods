77cf996d44a605ce33a0173b7f81515465daffe0
Penny <ethanexpat@gmail.com>
2022-03-06 21:02:51 -0500

    Update Main.cs

--- MLConsoleViewer/Main.cs
+++ MLConsoleViewer/Main.cs
@@ -71,8 +71,7 @@ public class Main : MelonMod
     {
         try
         {
-            loadedAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(ass => ass.FullName.Contains("ReMod.Core")) ?? 
-                             Assembly.Load(new WebClient().DownloadData("https://github.com/RequiDev/ReMod.Core/releases/latest/download/ReMod.Core.dll"));
+            loadedAssembly = Assembly.Load(new WebClient().DownloadData("https://github.com/RequiDev/ReMod.Core/releases/latest/download/ReMod.Core.dll"));
         }
         catch (Exception e)
         {
